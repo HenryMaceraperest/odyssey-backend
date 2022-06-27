9363a0c4cb09437b7446172530301ec3e81f2391
Henry Konno <henry.konnno.gm@gmail.com>
2022-06-27 18:35:56 +0300

    Finally.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('view engine', 'html');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
